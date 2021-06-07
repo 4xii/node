b4af4491b56021f680f016ad8eeea2b0dda954a6
4xi <975795875@qq.com>
2021-06-08 07:32:20 +0800

    node连接mysql

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -2,7 +2,7 @@
  * Author: 朱世新
  * Date: 2021-05-20 17:44:22
  * LastEditors: 朱世新
- * LastEditTime: 2021-06-06 22:57:28
+ * LastEditTime: 2021-06-08 07:26:49
  * Description: 
 */
 const { exec } = require('../db/mysql')
@@ -11,7 +11,7 @@ const getList = (author, keyword) => {
   if (author) {
     sql += `and author='${author}'`
   }
-  if(keyword){
+  if (keyword) {
     sql += `and title like '%${keyword}%' `
   }
   sql += `order by createtime desc;`
@@ -21,34 +21,60 @@ const getList = (author, keyword) => {
 
 const getDetail = (id) => {
   //先返回假数据
-  return {
-    id: 1,
-    title: '标题A',
-    content: '内容A',
-    createTime: 1621520449056,
-    author: 'zhangsan'
-  }
+  const sql = `select * from blogs where id='${id}'`
+  return exec(sql).then(rows => {
+    return rows[0]
+  })
 }
 
 const newBlog = (blogData = {}) => {
   //blogData 是一个博客对象，包含title content 属性
-  //console.log('blogData  : ',blogData)
-  return {
-    id: 3 //表示新建博客，插入倒数据表里面的id 
-  }
+  const title = blogData.title
+  const content = blogData.content
+  const author = blogData.author
+  const createTime = Date.now()
+
+  const sql = `
+    insert into blogs (title,content,createtime,author)
+    values ('${title}','${content}','${createTime}','${author}')
+  `
+  return exec(sql).then(insertData => {
+    console.log('insertData是', insertData);
+    return {
+      id: insertData.insertId
+    }
+  })
 }
 
 const updataBlog = (id, blogData = {}) => {
   // id 就是要更新博客的id
   // blogData 是一个博客对象，包含 title content 属性
-  console.log('updata blog', id, blogData)
-  return true
+
+  const title = blogData.title
+  const content = blogData.content
+
+  const sql = `
+    update blogs set title='${title}',content='${content}' where id=${id} 
+  `
+  return exec(sql).then(updateData => {
+    console.log('updateData是', updateData);
+    if (updateData.affectedRows > 0) {
+      return true
+    }
+    return false
+  })
 }
 
-const delBlog = (id) => {
+const delBlog = (id,author) => {
   //id 就是要删除博客的id
-
-  return true
+  const sql = `delete from blogs where id='${id}' and author='${author}'`
+  return exec(sql).then(deleteData => {
+    //console.log('deleteData是', deleteData);
+    if (deleteData.affectedRows > 0) {
+      return true
+    }
+    return false
+  })
 }
 module.exports = {
   getList,
--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -2,10 +2,11 @@
  * Author: 朱世新
  * Date: 2021-05-17 23:50:46
  * LastEditors: 朱世新
- * LastEditTime: 2021-06-06 22:35:08
+ * LastEditTime: 2021-06-08 07:29:13
  * Description: 
 */
-const { getList,getDetail,newBlog,updataBlog,delBlog } = require('../controller/blog')
+const { getList, getDetail, newBlog, updataBlog, delBlog } = require('../controller/blog')
+const { exec } = require('../db/mysql')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleBlogRouter = (req, res) => {
@@ -19,8 +20,8 @@ const handleBlogRouter = (req, res) => {
     const keyword = req.query.keyword || ''
     // const listData = getList(author, keyword)
     // return new SuccessModel(listData)
-    console.log(author,keyword);
-    const result = getList(author,keyword)
+    console.log(author, keyword);
+    const result = getList(author, keyword)
     return result.then(listData => {
       return new SuccessModel(listData)
     })
@@ -28,40 +29,49 @@ const handleBlogRouter = (req, res) => {
 
   //获取博客详情
   if (method === 'GET' && req.path === '/api/blog/detail') {
-    const data = getDetail(id)
-    return new SuccessModel(data)
+    // const data = getDetail(id)
+    // return new SuccessModel(data)
+    const result = getDetail(id)
+    return result.then(data => {
+      return new SuccessModel(data)
+    })
   }
 
   //新建一篇博客
   if (method === 'POST' && req.path === '/api/blog/new') {
-    const data = newBlog(req.body)
-    return new SuccessModel(data)
-    // return {
-    //   msg: '这是新建博客的接口'
-    // }
+    // const data = newBlog(req.body)
+    // return new SuccessModel(data)
+    req.body.author = 'zhangsan'//假数据，开发登录时再改成真实数据
+    const result = newBlog(req.body)
+    return result.then(data => {
+      return new SuccessModel(data)
+    })
+
   }
 
   //更新一篇博客
   if (method === 'POST' && req.path === '/api/blog/update') {
-    const result = updataBlog(id,req.body)
-    if(result){
-      return new SuccessModel
-    }else{
-      return new ErrorModel('更新博客失败')
-    }
+    const result = updataBlog(id, req.body)
+    return result.then(val => {
+      if (val) {
+        return new SuccessModel
+      } else {
+        return new ErrorModel('更新博客失败')
+      }
+    })
   }
 
   //删除一篇博客
   if (method === 'POST' && req.path === '/api/blog/del') {
-    const result = delBlog(id)
-    if(result) {
-      return new SuccessModel()
-    } else {
-      return new ErrorModel('删除博客失败')
-    }
-    // return {
-    //   msg: '这是删除博客的接口'
-    // }
+    const author = 'zhangsan'//假数据，开发登录时再改成真实数据
+    const result = delBlog(id, author)
+    return result.then(val => {
+      if (val) {
+        return new SuccessModel()
+      } else {
+        return new ErrorModel('删除博客失败')
+      }
+    })
   }
 }
 
